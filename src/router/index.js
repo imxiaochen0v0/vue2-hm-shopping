1cc4afb66bdec36b02af9e96f01e9c1c22939138
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-01-04 18:41:00 +0800

    全局前置守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store/index'
 
 Vue.use(VueRouter)
 
@@ -53,6 +54,29 @@ const router = new VueRouter({
       component: () => import('../views/search/list.vue')
     }
   ]
+})
+
+// 全局前置导航守卫 => 所有路由到真正被访问之前 都会先经过全局前置导航守卫
+//  to:  到哪去的完整路由对象(路径，参数)
+// from: 从哪的完整路由对象(路径，参数)
+// next: 是否放行
+// 1、 next() 直接放行
+// 2、next(路径) 进行拦截，拦截到next()里配置的路径
+
+const authUrls = ['/myOrder', '/pay']
+router.beforeEach((to, from, next) => {
+  // 非权限页面 直接放行
+  if (!authUrls.includes(to.path)) {
+    next()
+    return
+  }
+  // 权限页面 验证token
+  const token = store.getters.token
+  if (token) {
+    next()
+  } else {
+    next('/login')
+  }
 })
 
 export default router
