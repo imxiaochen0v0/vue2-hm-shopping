f75417dd55884b37488a3598f9b58417b1ba50d4
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-01-04 13:18:45 +0800

    添加请求loading效果

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,6 +10,12 @@ const instance = axios.create({
 // 添加请求拦截器
 instance.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
+  // 开启loading 禁止背景点击 (节流处理, 防止多次无效触发)
+  Toast.loading({
+    message: '加载中...',
+    forbidClick: true, // 禁止背景点击
+    duration: 0 // 持续展示 toast
+  })
   return config
 }, function (error) {
   // 对请求错误做些什么
@@ -21,9 +27,13 @@ instance.interceptors.response.use(function (response) {
   // 2xx 范围内的状态码都会触发该函数
   const res = response.data
   if (res.status !== 200) {
+    // Toast默认为单例 同时只能存在一个 新的覆盖旧的
     Toast(res.message)
     // 抛出一个错误的 promise
     return Promise.reject(res.message)
+  } else {
+    // 正确情况
+    Toast.clear()
   }
   // 对响应数据做点什么(axios会多包一层data)
   return response.data
